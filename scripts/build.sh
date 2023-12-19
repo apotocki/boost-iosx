06eb26cfd5ed0a9da7b56b4be922d783f373011e
apotocki <sane22222@aim.com>
2023-12-19 08:34:03 +0100

    version 1.84.0

--- scripts/build.sh
+++ scripts/build.sh
@@ -97,8 +97,7 @@ fi
 patch tools/build/src/tools/features/instruction-set-feature.jam $SCRIPT_DIR/instruction-set-feature.jam.patch
 
 
-# --with-cobalt
-LIBS_TO_BUILD="--with-atomic --with-chrono --with-container --with-context --with-contract --with-coroutine --with-date_time --with-exception --with-fiber --with-filesystem --with-graph --with-iostreams --with-json --with-locale --with-log --with-math --with-nowide --with-program_options --with-random --with-regex --with-serialization --with-stacktrace --with-system --with-test --with-thread --with-timer --with-type_erasure --with-wave --with-url"
+LIBS_TO_BUILD="--with-atomic --with-chrono --with-cobalt --with-container --with-context --with-contract --with-coroutine --with-date_time --with-exception --with-fiber --with-filesystem --with-graph --with-iostreams --with-json --with-locale --with-log --with-math --with-nowide --with-program_options --with-random --with-regex --with-serialization --with-stacktrace --with-system --with-test --with-thread --with-timer --with-type_erasure --with-wave --with-url"
 
 B2_BUILD_OPTIONS="-j$THREAD_COUNT address-model=64 release link=static runtime-link=shared define=BOOST_SPIRIT_THREADSAFE cxxflags=\"-std=c++20\""
 
@@ -267,7 +266,7 @@ build_xcframework()
 if true; then
 build_xcframework boost_atomic
 build_xcframework boost_chrono
-#build_xcframework boost_cobalt
+build_xcframework boost_cobalt
 build_xcframework boost_container
 build_xcframework boost_context
 build_xcframework boost_contract
